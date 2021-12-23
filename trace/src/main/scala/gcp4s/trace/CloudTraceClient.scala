27ba01e474c531d6256cc88c70eb385b369470e1
Arman Bilge <armanbilge@gmail.com>
2021-12-23 05:29:12 +0000

    Drop nulls from json encoder

--- trace/src/main/scala/gcp4s/trace/CloudTraceClient.scala
+++ trace/src/main/scala/gcp4s/trace/CloudTraceClient.scala
@@ -19,15 +19,24 @@ package gcp4s.trace
 import cats.effect.kernel.Concurrent
 import cats.syntax.all.*
 import gcp4s.trace.model.BatchWriteSpansRequest
+import io.circe.Encoder
+import org.http4s.EntityEncoder
 import org.http4s.Method.POST
-import org.http4s.circe.CirceEntityCodec.*
+import org.http4s.circe.CirceEntityDecoder
+import org.http4s.circe.CirceInstances
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.syntax.all.*
 
-final class CloudTraceClient[F[_]: Concurrent](client: Client[F], projectId: String)
-    extends Http4sClientDsl[F]:
+final private class CloudTraceClient[F[_]: Concurrent](client: Client[F], projectId: String)
+    extends Http4sClientDsl[F],
+      CirceInstances,
+      CirceEntityDecoder:
   private val endpoint = uri"https://cloudtrace.googleapis.com/v2/projects"
+
+  protected override val defaultPrinter = super.defaultPrinter.copy(dropNullValues = true)
+  private given [F[_], A: Encoder]: EntityEncoder[F, A] =
+    jsonEncoderOf[F, A]
 
   def batchWrite(spans: List[model.Span]): F[Unit] =
     client.expect(
